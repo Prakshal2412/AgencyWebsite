5aae40b81d2abb2c968cfc9de0653ddc01c57a4b
Prakshal Jain <Prakshal.net12345@gmail.com>
2024-06-25 23:46:48 +0530

    nav

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
       <a class="mr-5 hover:text-gray-900">Courses</a>
       <a class="mr-5 hover:text-gray-900">Resources</a>
       <a class="mr-5 hover:text-gray-900">Online Learning</a>
-      <a class="mr-5 hover:text-gray-900">ok</a>
+      <a class="mr-5 hover:text-gray-900">Contact us</a>
     </nav>
     <div className='gap-5 flex '>
     <button class="bg-green-200 inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Log In
